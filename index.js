bb10aeaf3a1c6223366a377e937de0f6cc63eef8
anik3tra0 <anik3tra0@gmail.com>
2017-06-28 15:23:21 +0530

    Upgraded to TCP IP Connection with WS Connection

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-var server = require('websocket').server, http = require('http');
+var webSocketServer = require('websocket').server, http = require('http');
+var net = require('net');
 
-var socket = new server({
+var tcpServer = net.createServer();
+tcpServer.on('connection', handleConnection);
+
+tcpServer.listen(9000, function() {
+  console.log('server listening to %j', tcpServer.address());
+});
+
+var socket = new webSocketServer({
   httpServer: http.createServer().listen(1337)
 });
 
@@ -19,6 +27,39 @@ socket.on('request', function(request) {
   });
 
   connection.on('close', function(connection) {
+    var index = connections.indexOf(connection);
+    connections.splice(index, 1);
+    console.log('connection from %s closed', remoteAddress);
+    console.log(connections);
     console.log('connection closed');
   });
 });
+
+
+function handleConnection(conn) {
+
+  var remoteAddress = conn.remoteAddress + ':' + conn.remotePort;
+  console.log('new client connection from %s', remoteAddress);
+
+  conn.on('data', onConnData);
+  conn.once('close', onConnClose);
+  conn.on('error', onConnError);
+
+  function onConnData(d) {
+    console.log('connection data from %s: %j', remoteAddress, d);
+    for (i = 0; i < connections.length; i++) {
+      connections[i].sendUTF(d);
+    }
+  }
+
+  function onConnClose() {
+    var index = connections.indexOf(conn);
+    connections.splice(index, 1);
+    console.log('connection from %s closed', remoteAddress);
+    console.log(connections);
+  }
+
+  function onConnError(err) {
+    console.log('Connection %s error: %s', remoteAddress, err.message);
+  }
+}
